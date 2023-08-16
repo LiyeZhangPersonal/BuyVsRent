9101555a4f7a07eef15d969385ed1d3434fc2244
LiyeZhangPersonal <lee1026@gmail.com>
2023-08-16 12:56:42 -0400

    Now feature complete against the static calculators

--- buy_vs_rent_calculator.js
+++ buy_vs_rent_calculator.js
@@ -1,63 +1,75 @@
-function calculateBuyVsRent() {
-    // Retrieve values from the form
-    var homePrice = parseFloat(document.getElementById('homePrice').value.replace(/,/g, ''));
-    var downPayment = parseFloat(document.getElementById('downPayment').value.replace(/,/g, ''));
-    var loanTerm = parseInt(document.getElementById('loanTerm').value);
-    var interestRate = parseFloat(document.getElementById('interestRate').value.replace(/,/g, '')) / 100 / 12;
-    var homeAppreciationRate = parseFloat(document.getElementById('homeAppreciationRate').value.replace(/,/g, '')) / 100;
-    var sellingCosts = parseFloat(document.getElementById('sellingCosts').value.replace(/,/g, '')) / 100;
-    var propertyTaxRate = parseFloat(document.getElementById('propertyTaxRate').value.replace(/,/g, '')) / 100;
-    var initialBuyingCosts = parseFloat(document.getElementById('initialBuyingCosts').value.replace(/,/g, '')) / 100;
-    var annualInsuranceCosts = parseFloat(document.getElementById('annualInsuranceCosts').value.replace(/,/g, '')) / 100;
-    var annualMaintenanceCosts = parseFloat(document.getElementById('annualMaintenanceCosts').value.replace(/,/g, '')) / 100;
-    var monthlyCommonCharges = parseFloat(document.getElementById('monthlyCommonCharges').value.replace(/,/g, ''));
-    var monthlyRent = parseFloat(document.getElementById('monthlyRent').value.replace(/,/g, ''));
-    var annualRentIncrease = parseFloat(document.getElementById('annualRentIncrease').value.replace(/,/g, '')) / 100;
-    var marginalTaxRate = parseFloat(document.getElementById('marginalTaxRate').value.replace(/,/g, '')) / 100;
-    var saltCapRemaining = parseFloat(document.getElementById('saltCapRemaining').value.replace(/,/g, ''));
-    var filingMarried = document.getElementById('filingMarried').checked;
+function formatNumber(num) {
+  return num.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
-    // Additional calculations
-    var loanAmount = homePrice - downPayment;
-    var monthlyMortgagePayment = loanAmount * (interestRate * Math.pow(1 + interestRate, loanTerm * 12)) / (Math.pow(1 + interestRate, loanTerm * 12) - 1);
-    var currentYearlyRent = monthlyRent * 12;
+function calculateBuyVsRent() {
+    // Retrieve input values
+    var homePrice = parseFloat(document.getElementById("homePrice").value.replace(/,/g, ''));
+    var downPayment = parseFloat(document.getElementById("downPayment").value.replace(/,/g, ''));
+    var loanTerm = parseInt(document.getElementById("loanTerm").value);
+    var interestRate = parseFloat(document.getElementById("interestRate").value) / 100;
+    var monthlyRent = parseFloat(document.getElementById("monthlyRent").value.replace(/,/g, ''));
+    var homeAppreciationRate = parseFloat(document.getElementById("homeAppreciationRate").value) / 100;
+    var sellingCostsRate = parseFloat(document.getElementById("sellingCosts").value) / 100;
+    var propertyTaxRate = parseFloat(document.getElementById("propertyTaxRate").value) / 100;
+    var initialBuyingCostsRate = parseFloat(document.getElementById("initialBuyingCosts").value) / 100;
+    var annualInsuranceCostsRate = parseFloat(document.getElementById("annualInsuranceCosts").value) / 100;
+    var annualMaintenanceCostsRate = parseFloat(document.getElementById("annualMaintenanceCosts").value) / 100;
+    var monthlyCommonCharges = parseFloat(document.getElementById("monthlyCommonCharges").value.replace(/,/g, ''));
+    var annualRentIncrease = parseFloat(document.getElementById("annualRentIncrease").value) / 100;
+    var investmentInterestRate = parseFloat(document.getElementById("investmentInterestRate").value) / 100;
+    var marginalTaxRate = parseFloat(document.getElementById("marginalTaxRate").value) / 100;
+    var saltCapRemaining = parseFloat(document.getElementById("saltCapRemaining").value.replace(/,/g, ''));
+    var filingMarried = document.getElementById("filingMarried").checked;
+
+    // Other variables
+    var remainingLoanBalance = homePrice - downPayment;
+    var monthlyInterestRate = interestRate / 12;
+    var monthlyPayment = remainingLoanBalance * (monthlyInterestRate + (monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, loanTerm * 12) - 1)));
+
+    // Result variables
+    var cumulativeBuyingCost = downPayment + (homePrice * initialBuyingCostsRate);
     var cumulativeRent = 0;
-    var cumulativeBuyingCost = downPayment + (homePrice * initialBuyingCosts);
-    var remainingLoanBalance = loanAmount;
+    var investmentAccountBalance = 0;
+	
+    // Initial investment account balance
+    var initialInvestment = downPayment + (homePrice * initialBuyingCostsRate) - monthlyRent * 12;
+    var investmentAccountBalance = initialInvestment;
 
     // Result table
-    var resultTable = '<table><tr><th>Year</th><th>Buy Outlay</th><th>Home Price</th><th>Mortgage Balance</th><th>Home Equity</th><th>Rent Paid</th><th>Total Cost after Selling</th><th>Net Savings from Buying</th><th>Tax Savings</th></tr>';
+    var resultTable = '<table><tr><th>Year</th><th>Buy Outlay</th><th>Home Price</th><th>Mortgage Balance</th><th>Home Equity</th><th>Rent Paid</th><th>Tax Savings</th><th>Investment Account</th><th>Net Proceeds After Sale</th><th>Investment vs Sale Proceeds Difference</th></tr>';
 
     for (var year = 1; year <= loanTerm; year++) {
-        var yearlyRent = currentYearlyRent * Math.pow(1 + annualRentIncrease, year - 1);
-        cumulativeRent += yearlyRent;
+        var interestPaid = remainingLoanBalance * interestRate;
+        var propertyTaxesPaid = homePrice * propertyTaxRate;
+        var annualInsuranceCosts = homePrice * annualInsuranceCostsRate;
+        var annualMaintenanceCosts = homePrice * annualMaintenanceCostsRate;
 
-        var yearlyMortgage = monthlyMortgagePayment * 12;
-        var interestPaidForYear = 0;
-        for (var month = 1; month <= 12; month++) {
-            var interestPaidThisMonth = remainingLoanBalance * interestRate;
-            interestPaidForYear += interestPaidThisMonth;
-            remainingLoanBalance -= (monthlyMortgagePayment - interestPaidThisMonth);
-        }
-        var yearlyPropertyTax = homePrice * propertyTaxRate;
-        var yearlyInsurance = homePrice * annualInsuranceCosts;
-        var yearlyMaintenance = homePrice * annualMaintenanceCosts;
-        yearlyMortgage += yearlyPropertyTax + yearlyInsurance + yearlyMaintenance + (monthlyCommonCharges * 12);
+        var annualBuyingCost = monthlyPayment * 12 + propertyTaxesPaid + annualInsuranceCosts + annualMaintenanceCosts + monthlyCommonCharges * 12;
+        cumulativeBuyingCost += annualBuyingCost;
 
-        var taxSavings = calculateTaxSavings(interestPaidForYear, yearlyPropertyTax, saltCapRemaining, marginalTaxRate, filingMarried, interestRate);
-        cumulativeBuyingCost += yearlyMortgage;
+        var taxSavings = calculateTaxSavings(interestPaid, propertyTaxesPaid, saltCapRemaining, marginalTaxRate, filingMarried, monthlyInterestRate);
         cumulativeBuyingCost -= taxSavings;
 
-        homePrice *= (1 + homeAppreciationRate);
-        var homeEquity = homePrice - remainingLoanBalance;
-        var totalCostAfterSelling = cumulativeBuyingCost - (homeEquity - (homePrice * sellingCosts));
-        var netSavingsFromBuying = cumulativeRent - totalCostAfterSelling;
+        var annualRentCost = monthlyRent * 12;
+        cumulativeRent += annualRentCost;
 
-        resultTable += '<tr><td>' + year + '</td><td>' + cumulativeBuyingCost.toFixed(2) + '</td><td>' + homePrice.toFixed(2) + '</td><td>' + remainingLoanBalance.toFixed(2) + '</td><td>' + homeEquity.toFixed(2) + '</td><td>' + cumulativeRent.toFixed(2) + '</td><td>' + totalCostAfterSelling.toFixed(2) + '</td><td>' + netSavingsFromBuying.toFixed(2) + '</td><td>' + taxSavings.toFixed(2) + '</td></tr>';
+        var netAnnualDifference = annualBuyingCost - annualRentCost - taxSavings;
+        investmentAccountBalance = (investmentAccountBalance + netAnnualDifference) * (1 + investmentInterestRate);
+
+        var homeEquity = homePrice - remainingLoanBalance;
+        var sellingCosts = homePrice * sellingCostsRate;
+        var netProceedsAfterSale = homePrice - sellingCosts - remainingLoanBalance;
+        var investmentVsSaleProceedsDifference = investmentAccountBalance - netProceedsAfterSale;
+
+		resultTable += '<tr><td>' + year + '</td><td>' + formatNumber(cumulativeBuyingCost) + '</td><td>' + formatNumber(homePrice) + '</td><td>' + formatNumber(remainingLoanBalance) + '</td><td>' + formatNumber(homeEquity) + '</td><td>' + formatNumber(cumulativeRent) + '</td><td>' + formatNumber(taxSavings) + '</td><td>' + formatNumber(investmentAccountBalance) + '</td><td>' + formatNumber(netProceedsAfterSale) + '</td><td>' + formatNumber(investmentVsSaleProceedsDifference) + '</td></tr>';
+
+        // Update for next iteration
+        homePrice *= (1 + homeAppreciationRate);
+        monthlyRent *= (1 + annualRentIncrease);
+        remainingLoanBalance -= (monthlyPayment * 12 - interestPaid);
     }
 
     resultTable += '</table>';
-
-    // Display the result
-    document.getElementById('result').innerHTML = resultTable;
+    document.getElementById("result").innerHTML = resultTable;
 }
--- tax_calculations.js
+++ tax_calculations.js
@@ -1,17 +1,20 @@
 function calculateTaxSavings(interestPaid, propertyTaxesPaid, saltCapRemaining, marginalTaxRate, filingMarried, interestRate) {
+    // Constants
     var standardDeduction = filingMarried ? 27700 : 13850;
     var saltCap = 10000;
-    var maxMortgageDebtForInterestDeduction = 750000;
+    var maxMortgageDebtForInterestDeduction = 750000; // Apply interest deduction only to the first $750,000 of mortgage debt
 
-    // Apply interest deduction only to the first $750,000 of mortgage debt
-    var deductibleInterest = Math.min(interestPaid, maxMortgageDebtForInterestDeduction * interestRate);
+    // Calculate deductible interest
+    var deductibleInterest = Math.min(interestPaid, maxMortgageDebtForInterestDeduction * interestRate * 12);
 
-    var reductionInTaxableIncome =
-        deductibleInterest +
-        (Math.min(saltCapRemaining, propertyTaxesPaid)) -
-        (standardDeduction - saltCap + saltCapRemaining);
+    // Calculate SALT deduction
+    var saltDeduction = Math.min(saltCapRemaining, propertyTaxesPaid);
 
-    var taxSavings = reductionInTaxableIncome * marginalTaxRate / 100;
+    // Calculate reduction in taxable income
+    var reductionInTaxableIncome = (deductibleInterest + saltDeduction + saltCap) - standardDeduction;
 
-    return Math.max(taxSavings, 0); // Ensure tax savings are non-negative
+    // Calculate tax savings
+    var taxSavings = reductionInTaxableIncome * marginalTaxRate;
+
+    return taxSavings > 0 ? taxSavings : 0; // Ensure tax savings are non-negative
 }
