695d98a19e64f14b35832a4905f56ffcf5298fd4
LiyeZhangPersonal <lee1026@gmail.com>
2023-08-16 11:30:34 -0400

    Adding taxes and annual tax increases

--- buy_vs_rent_calculator.js
+++ buy_vs_rent_calculator.js
@@ -1,74 +1,63 @@
 function calculateBuyVsRent() {
+    // Retrieve values from the form
     var homePrice = parseFloat(document.getElementById('homePrice').value.replace(/,/g, ''));
     var downPayment = parseFloat(document.getElementById('downPayment').value.replace(/,/g, ''));
-    var loanTermYears = parseFloat(document.getElementById('loanTerm').value.replace(/,/g, ''));
-    var loanTerm = loanTermYears * 12;
+    var loanTerm = parseInt(document.getElementById('loanTerm').value);
     var interestRate = parseFloat(document.getElementById('interestRate').value.replace(/,/g, '')) / 100 / 12;
-    var monthlyRent = parseFloat(document.getElementById('monthlyRent').value.replace(/,/g, ''));
-    var appreciationRate = parseFloat(document.getElementById('appreciationRate').value.replace(/,/g, '')) / 100;
+    var homeAppreciationRate = parseFloat(document.getElementById('homeAppreciationRate').value.replace(/,/g, '')) / 100;
     var sellingCosts = parseFloat(document.getElementById('sellingCosts').value.replace(/,/g, '')) / 100;
     var propertyTaxRate = parseFloat(document.getElementById('propertyTaxRate').value.replace(/,/g, '')) / 100;
     var initialBuyingCosts = parseFloat(document.getElementById('initialBuyingCosts').value.replace(/,/g, '')) / 100;
     var annualInsuranceCosts = parseFloat(document.getElementById('annualInsuranceCosts').value.replace(/,/g, '')) / 100;
     var annualMaintenanceCosts = parseFloat(document.getElementById('annualMaintenanceCosts').value.replace(/,/g, '')) / 100;
     var monthlyCommonCharges = parseFloat(document.getElementById('monthlyCommonCharges').value.replace(/,/g, ''));
-    var marginalTaxRate = parseFloat(document.getElementById('marginalTaxRate').value.replace(/,/g, ''));
+    var monthlyRent = parseFloat(document.getElementById('monthlyRent').value.replace(/,/g, ''));
+    var annualRentIncrease = parseFloat(document.getElementById('annualRentIncrease').value.replace(/,/g, '')) / 100;
+    var marginalTaxRate = parseFloat(document.getElementById('marginalTaxRate').value.replace(/,/g, '')) / 100;
     var saltCapRemaining = parseFloat(document.getElementById('saltCapRemaining').value.replace(/,/g, ''));
     var filingMarried = document.getElementById('filingMarried').checked;
 
+    // Additional calculations
     var loanAmount = homePrice - downPayment;
-    var monthlyMortgage = (loanAmount * interestRate * Math.pow(1 + interestRate, loanTerm)) / (Math.pow(1 + interestRate, loanTerm) - 1);
-    var additionalBuyingCosts = homePrice * initialBuyingCosts;
-
-    var breakdown = "<h3>Yearly Breakdown</h3><table border='1'><tr><th>Year</th><th>Buy Outlay</th><th>Home Price</th><th>Mortgage Balance</th><th>Home Equity</th><th>Rent Paid</th><th>Total Cost After Selling</th><th>Net Savings from Buying</th><th>Tax Savings</th></tr>";
-    var cumulativeBuyingCost = downPayment + additionalBuyingCosts;
-    var cumulativeRentingCost = 0;
+    var monthlyMortgagePayment = loanAmount * (interestRate * Math.pow(1 + interestRate, loanTerm * 12)) / (Math.pow(1 + interestRate, loanTerm * 12) - 1);
+    var currentYearlyRent = monthlyRent * 12;
+    var cumulativeRent = 0;
+    var cumulativeBuyingCost = downPayment + (homePrice * initialBuyingCosts);
     var remainingLoanBalance = loanAmount;
 
-    for (var year = 1; year <= loanTermYears; year++) {
-        var homeValue = homePrice * Math.pow(1 + appreciationRate, year);
-        var yearlyPropertyTax = homeValue * propertyTaxRate;
-        var yearlyInsuranceCost = homeValue * annualInsuranceCosts;
-        var yearlyMaintenanceCost = homeValue * annualMaintenanceCosts;
-        var yearlyCommonCharges = monthlyCommonCharges * 12;
-        var yearlyMortgage = (monthlyMortgage + yearlyPropertyTax / 12) * 12 + yearlyInsuranceCost + yearlyMaintenanceCost + yearlyCommonCharges;
-        var yearlyRent = monthlyRent * 12;
+    // Result table
+    var resultTable = '<table><tr><th>Year</th><th>Buy Outlay</th><th>Home Price</th><th>Mortgage Balance</th><th>Home Equity</th><th>Rent Paid</th><th>Total Cost after Selling</th><th>Net Savings from Buying</th><th>Tax Savings</th></tr>';
 
+    for (var year = 1; year <= loanTerm; year++) {
+        var yearlyRent = currentYearlyRent * Math.pow(1 + annualRentIncrease, year - 1);
+        cumulativeRent += yearlyRent;
+
+        var yearlyMortgage = monthlyMortgagePayment * 12;
         var interestPaidForYear = 0;
         for (var month = 1; month <= 12; month++) {
-            var interestPayment = remainingLoanBalance * interestRate;
-            interestPaidForYear += interestPayment;
-            remainingLoanBalance -= (monthlyMortgage - interestPayment);
+            var interestPaidThisMonth = remainingLoanBalance * interestRate;
+            interestPaidForYear += interestPaidThisMonth;
+            remainingLoanBalance -= (monthlyMortgagePayment - interestPaidThisMonth);
         }
+        var yearlyPropertyTax = homePrice * propertyTaxRate;
+        var yearlyInsurance = homePrice * annualInsuranceCosts;
+        var yearlyMaintenance = homePrice * annualMaintenanceCosts;
+        yearlyMortgage += yearlyPropertyTax + yearlyInsurance + yearlyMaintenance + (monthlyCommonCharges * 12);
 
-		var taxSavings = calculateTaxSavings(interestPaidForYear, yearlyPropertyTax, saltCapRemaining, marginalTaxRate, filingMarried, remainingLoanBalance, interestRate);
-        cumulativeBuyingCost += yearlyMortgage - taxSavings;
-        cumulativeRentingCost += yearlyRent;
+        var taxSavings = calculateTaxSavings(interestPaidForYear, yearlyPropertyTax, saltCapRemaining, marginalTaxRate, filingMarried, interestRate);
+        cumulativeBuyingCost += yearlyMortgage;
+        cumulativeBuyingCost -= taxSavings;
 
-        var homeEquity = homeValue - remainingLoanBalance;
-        var totalCostAfterSelling = cumulativeBuyingCost - (homeEquity * (1 - sellingCosts));
-        var netSavingsFromBuying = cumulativeRentingCost - totalCostAfterSelling;
+        homePrice *= (1 + homeAppreciationRate);
+        var homeEquity = homePrice - remainingLoanBalance;
+        var totalCostAfterSelling = cumulativeBuyingCost - (homeEquity - (homePrice * sellingCosts));
+        var netSavingsFromBuying = cumulativeRent - totalCostAfterSelling;
 
-        breakdown += "<tr><td>" + year + "</td><td>$" + cumulativeBuyingCost.toFixed(2) +
-                     "</td><td>$" + homeValue.toFixed(2) + "</td><td>$" + remainingLoanBalance.toFixed(2) +
-                     "</td><td>$" + homeEquity.toFixed(2) + "</td><td>$" + cumulativeRentingCost.toFixed(2) +
-                     "</td><td>$" + totalCostAfterSelling.toFixed(2) + "</td><td>$" + netSavingsFromBuying.toFixed(2) +
-                     "</td><td>$" + taxSavings.toFixed(2) + "</td></tr>";
+        resultTable += '<tr><td>' + year + '</td><td>' + cumulativeBuyingCost.toFixed(2) + '</td><td>' + homePrice.toFixed(2) + '</td><td>' + remainingLoanBalance.toFixed(2) + '</td><td>' + homeEquity.toFixed(2) + '</td><td>' + cumulativeRent.toFixed(2) + '</td><td>' + totalCostAfterSelling.toFixed(2) + '</td><td>' + netSavingsFromBuying.toFixed(2) + '</td><td>' + taxSavings.toFixed(2) + '</td></tr>';
     }
 
-    breakdown += "</table>";
+    resultTable += '</table>';
 
-    var finalHomeValue = homePrice * Math.pow(1 + appreciationRate, loanTermYears);
-    var totalSellingCosts = finalHomeValue * sellingCosts;
-    var netProceeds = finalHomeValue - totalSellingCosts - remainingLoanBalance;
-
-    var totalCostBuying = downPayment + additionalBuyingCosts + monthlyMortgage * loanTerm - netProceeds + (finalHomeValue * propertyTaxRate * loanTermYears);
-    var totalCostRenting = monthlyRent * loanTerm;
-
-    var result = totalCostBuying < totalCostRenting ? "Buying is better" : "Renting is better";
-
-    document.getElementById('result').innerHTML = result + "<br>" +
-                                                   "Total cost of buying: $" + totalCostBuying.toFixed(2) + "<br>" +
-                                                   "Total cost of renting: $" + totalCostRenting.toFixed(2) +
-                                                   breakdown;
+    // Display the result
+    document.getElementById('result').innerHTML = resultTable;
 }
